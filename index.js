d24c10ded3c85a87d29be7561fb850bdce08feae
JothikaGit <jothika1050@gmail.com>
2024-12-18 22:06:28 +0530

    update project

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById('closeOffer').addEventListener('click', function () {
+  document.getElementById('offerBanner').style.display = 'none';
+});
+
+
+ddocument.getElementById('heart').addEventListener('click', function () {
+  this.classList.toggle('red-heart');
+  this.classList.toggle('black-heart');
+});
+
+
